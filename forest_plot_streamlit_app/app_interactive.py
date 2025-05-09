5e6a18fbb35278a60276480d1e4fc9a7bf4d9cbc
Hanin Ayadi <Hanin.Ayadi@lih.lu>
2025-05-09 12:50:17 +0200

    Double tab visualization

--- forest_plot_streamlit_app/app_interactive.py
+++ forest_plot_streamlit_app/app_interactive.py
@@ -19,7 +19,7 @@ SOCIO_VARIABLES = {
 # ----------------------
 # Sidebar Menu
 # ----------------------
-st.sidebar.title("Controls for joint forest plot and heatmap viz 🌲")
+st.sidebar.title("Visualization Controls 📊")
 st.sidebar.caption("Select parameters")
 
 
@@ -27,55 +27,58 @@ audio_types = ['reading', 'a_vowel phonation']
 feature_set_reading = ['egemaps', 'articulation', 'phonological', 'phonation', 'prosody', 'glottal'] 
 feature_set_phonation = ['egemaps', 'phonation', 'glottal'] 
 socio_factors = list(SOCIO_VARIABLES.keys())
-show_heatmap = st.sidebar.checkbox("Display Heatmap")
-
-# User input: select the sociodemographic variable
-
 audio_type = st.sidebar.selectbox("Choose an audio type:", audio_types)
-if audio_type == 'a_vowel phonation':
-    audio_type = 'a_vowel'
-    feature_set = st.sidebar.selectbox("Choose a feature set:", feature_set_phonation)
-elif audio_type == 'reading':
-    feature_set = st.sidebar.selectbox("Choose a feature set:", feature_set_reading)
 
-socio_var = st.sidebar.selectbox("Choose a sociodemographic variable:", socio_factors)
-gender_var = st.sidebar.selectbox("Choose gender variable:", ['female', 'male'])
+tab1, tab2 = st.tabs(["Forest Plots", "More visualization"])
 
-filename = f'{audio_type}_{feature_set}_{gender_var}.npy'
-feature_columns = f'{feature_set}_columns_to_work_with.npy'
+with tab1:
 
-if gender_var == 'female':
-    gender_des = 'women'
-elif gender_var == 'male':
-    gender_des = 'men'
+    if audio_type == 'a_vowel phonation':
+        audio_type = 'a_vowel'
+        feature_set = st.sidebar.selectbox("Choose a feature set:", feature_set_phonation)
+    elif audio_type == 'reading':
+        feature_set = st.sidebar.selectbox("Choose a feature set:", feature_set_reading)
 
-# ----------------------
-# Main Area
-# ----------------------
+    socio_var = st.sidebar.selectbox("Choose a sociodemographic variable:", socio_factors)
+    gender_var = st.sidebar.selectbox("Choose gender variable:", ['female', 'male'])
 
-st.title(f"The effect of {socio_var} on the {feature_set} features for {gender_des} 📊 ")
+    filename = f'{audio_type}_{feature_set}_{gender_var}.npy'
+    feature_columns = f'{feature_set}_columns_to_work_with.npy'
 
-# Generate plot
-fig = generate_plotly_forest_plot(
-    input_filename = os.path.join(DATA_FOLDER, filename),
-    columns_to_work_with = os.path.join(DATA_FOLDER, feature_columns),
-    socio_factors = socio_factors,
-    coef_name = socio_var,
-    feature_name = feature_set,
-    gender = gender_var)
+    if gender_var == 'female':
+        gender_des = 'women'
+    elif gender_var == 'male':
+        gender_des = 'men'
 
-heatmap_filename = f"{audio_type}_{feature_set}_{gender_var}_{SOCIO_VARIABLES[socio_var]}.jpg"
-heatmap_path = os.path.join(HEATMAP_FOLDER, heatmap_filename)
-# Load an image using Pillow
-if os.path.exists(heatmap_path):
-    heatmap_image = Image.open(heatmap_path)
-else:
-    heatmap_image = os.path.join(HEATMAP_FOLDER, 'nothing_to_see_here.gif')
+    # ----------------------
+    # Main Area
+    # ----------------------
+
+    st.title(f"The effect of {socio_var} on the {feature_set} features for {gender_des} ")
+
+    # Generate plot
+    fig = generate_plotly_forest_plot(
+        input_filename = os.path.join(DATA_FOLDER, filename),
+        columns_to_work_with = os.path.join(DATA_FOLDER, feature_columns),
+        socio_factors = socio_factors,
+        coef_name = socio_var,
+        feature_name = feature_set,
+        gender = gender_var)
+    # Display
+    st.plotly_chart(fig, use_container_width=True)
 
 
-# Display
-st.plotly_chart(fig, use_container_width=True)
+with tab2:
 
-if show_heatmap:
-    # Heatmap in sidebar
-    st.sidebar.image(heatmap_image, caption="Correlation Map", use_container_width=True)
+    heatmap_filename = f"{audio_type}_{feature_set}_{gender_var}_{SOCIO_VARIABLES[socio_var]}.jpg"
+    heatmap_path = os.path.join(HEATMAP_FOLDER, heatmap_filename)
+    # Load an image using Pillow
+    if os.path.exists(heatmap_path):
+        heatmap_image = Image.open(heatmap_path)
+    else:
+        heatmap_image = os.path.join(HEATMAP_FOLDER, 'nothing_to_see_here.gif')
+
+
+    #if show_heatmap:
+        # Heatmap in sidebar
+    st.image(heatmap_image, caption="Correlation Map", use_container_width=True)
